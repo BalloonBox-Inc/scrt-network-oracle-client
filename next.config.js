5984374b2a2e067e5b889af23923c912de15cc8f
mattdm3 <mortellitimatteo@gmail.com>
2022-03-09 15:08:02 -0500

    next config fixes

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
+/** @type {import('next').NextConfig} */
 module.exports = {
-  webpack5: true,
-  swcMinify: true,
+  reactStrictMode: true,
+  env: {
+    PLAID_CLIENT_ID: process.env.PLAID_CLIENT_ID,
+    PLAID_URL_SANDBOX: process.env.PLAID_URL_SANDBOX,
+    PLAID_SECRET_KEY_SANDBOX: process.env.PLAID_SECRET_KEY_SANDBOX,
+    COINBASE_CLIENT_ID: process.env.COINBASE_CLIENT_ID,
+    COINBASE_CLIENT_SECRET: process.env.COINBASE_CLIENT_SECRET,
+    COINBASE_BASE_AUTHORIZE_URL: process.env.COINBASE_BASE_AUTHORIZE_URL,
+    COINBASE_ACCESS_TOKEN_URL: process.env.COINBASE_ACCESS_TOKEN_URL,
+  },
 };
