d2e90d4ea4793b07bb731e0fe75e78b6630912eb
Yoon Kim <82831286+yoon-bbox@users.noreply.github.com>
2022-04-28 16:20:35 -0700

    added a testnet toggle & banner

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,5 +1,10 @@
 import React, { useState, useEffect, useCallback } from 'react';
 
+import {
+  WarningOutlined,
+  MenuOutlined,
+  CloseOutlined,
+} from '@ant-design/icons';
 import { motion } from 'framer-motion';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
@@ -11,6 +16,8 @@ export default function Header() {
   const { secretAddress } = useSecretContext();
   const [showWallet, setShowWallet] = useState<boolean>(false);
   const [shrinkHeader, setShrinkHeader] = useState<boolean>(false);
+  const [windowSize, setWindowSize] = useState<number | null>(null);
+  const [openMenu, setOpenMenu] = useState<boolean>(false);
   const router = useRouter();
   const routerIsMain =
     router?.pathname === '/' || router?.pathname === '/learn';
@@ -30,57 +37,161 @@ export default function Header() {
     return () => window.removeEventListener('scroll', handleScroll);
   }, [handleScroll]);
 
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowSize(window.innerWidth);
+    };
+
+    if (typeof window !== undefined) {
+      window.addEventListener('resize', handleResize);
+    }
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
   return (
-    <motion.header
-      style={{
-        height: shrinkHeader ? '60px' : '80px',
-        transition: '0.4s',
+    <>
+      <motion.header
+        style={{
+          height: shrinkHeader ? '60px' : '80px',
+          transition: '0.4s',
 
-        zIndex: '100',
-      }}
-      className={`sticky top-0 ${
-        shrinkHeader ? 'px-10 bg-black' : 'px-5'
-      } relative z-10 w-full font-sans py-0 flex items-center justify-between text-white ${
-        !routerIsMain && 'bg-black border-gray-800 border-b-2'
-      } `}
-    >
-      <Link passHref={true} href="/">
-        <a className="mt-1">
-          <img
-            src={'/images/scrt-logo-horizaontal-white.svg'}
-            width={140}
-            height={100}
-            alt="scrtsibyl-logo"
-            className={`cursor-pointer ${
-              showWallet ? 'disappear' : 'reappear'
-            }`}
-          />
-        </a>
-      </Link>
-
-      <div className="flex items-center min-h-full">
-        <Link passHref={true} href="/learn">
-          <p
-            className={`mr-5 text-xs sm:text-sm md:text-base text-center cursor-pointer hover:text-gray-400 ${
-              showWallet ? 'disappear' : 'reappear'
-            }`}
-          >
-            Learn
-          </p>
-        </Link>
-        {secretAddress && (
-          <Link passHref={true} href={'/start'}>
-            <p
-              className={`mr-5 text-xs sm:text-sm md:text-base text-center cursor-pointer hover:text-gray-400 ${
+          zIndex: '100',
+        }}
+        className={`sticky top-0 ${
+          shrinkHeader ? 'px-10 bg-black' : 'px-5'
+        } relative z-10 w-full font-sans py-0 flex items-center justify-between text-white ${
+          !routerIsMain && 'bg-black border-gray-800 border-b-2'
+        } `}
+      >
+        <Link passHref={true} href="/">
+          <a className="mt-1">
+            <img
+              src={'/images/scrt-logo-horizaontal-white.svg'}
+              width={140}
+              height={100}
+              alt="scrtsibyl-logo"
+              className={`cursor-pointer ${
                 showWallet ? 'disappear' : 'reappear'
               }`}
+            />
+          </a>
+        </Link>
+
+        <div className="flex items-center min-h-full">
+          {windowSize && windowSize < 500 ? (
+            <div
+              style={{ fontSize: '1.5rem', marginRight: '1rem' }}
+              onClick={() => setOpenMenu(!openMenu)}
+              className={`${
+                showWallet ? 'disappear' : 'appear'
+              } cursor-pointer hover:text-gray-400`}
             >
-              Start
-            </p>
-          </Link>
-        )}
-        <Connect showWallet={showWallet} setShowWallet={setShowWallet} />
+              {openMenu ? <CloseOutlined /> : <MenuOutlined />}
+            </div>
+          ) : (
+            <div className={`flex items-center`}>
+              <div
+                className={`bg-purple/40 text-xs sm:text-sm mr-4 px-3 py-2 rounded-lg ${
+                  showWallet ? 'disappear' : 'reappear'
+                }`}
+              >
+                {' '}
+                <a
+                  href={`${
+                    process.env.NODE_ENV === 'development'
+                      ? 'https://www.secretsibyl.com/'
+                      : 'https://www.test.secretsibyl.com'
+                  }`}
+                >
+                  {process.env.NODE_ENV === 'development'
+                    ? 'Switch to mainnet'
+                    : 'Switch to testnet'}
+                </a>
+              </div>
+              <Link passHref={true} href="/learn">
+                <p
+                  className={`mr-5 text-xs sm:text-sm md:text-base text-center cursor-pointer hover:text-gray-400 ${
+                    showWallet ? 'disappear' : 'reappear'
+                  }`}
+                >
+                  Learn
+                </p>
+              </Link>
+              {secretAddress && (
+                <Link passHref={true} href={'/start'}>
+                  <p
+                    className={`mr-5 text-xs sm:text-sm md:text-base text-center cursor-pointer hover:text-gray-400 ${
+                      showWallet ? 'disappear' : 'reappear'
+                    }`}
+                  >
+                    Start
+                  </p>
+                </Link>
+              )}
+            </div>
+          )}
+          <Connect showWallet={showWallet} setShowWallet={setShowWallet} />
+        </div>
+      </motion.header>
+
+      {windowSize && windowSize < 500 && openMenu && (
+        <>
+          <div
+            className={`absolute top-20 flex flex-col w-full items-center z-20`}
+          >
+            <div
+              className={`w-full text-center py-5 bg-black hover:bg-neutral-800 cursor-pointer`}
+            >
+              {' '}
+              <a
+                href={`${
+                  process.env.NODE_ENV === 'development'
+                    ? 'https://www.secretsibyl.com/'
+                    : 'https://www.test.secretsibyl.com'
+                }`}
+              >
+                {process.env.NODE_ENV === 'development'
+                  ? 'Switch to mainnet'
+                  : 'Switch to testnet'}
+              </a>
+            </div>
+
+            <div
+              className={`w-full text-center py-5 bg-black hover:bg-neutral-800 cursor-pointer`}
+            >
+              Learn
+            </div>
+
+            {secretAddress && (
+              <div
+                className={`w-full text-center py-5 bg-black hover:bg-neutral-800 cursor-pointer`}
+              >
+                Start
+              </div>
+            )}
+          </div>
+        </>
+      )}
+      <div className="sticky w-full bg-purple/60 z-10 flex sm:flex-row flex-col justify-center items-center py-4">
+        <WarningOutlined
+          style={{ fontSize: '1.1rem', marginRight: '0.4rem' }}
+        />{' '}
+        <p className="text-center sm:text-sm text-xs">
+          This is a testnet environment. The funds are not real. &nbsp;
+        </p>
+        <a
+          href={`${
+            process.env.NODE_ENV === 'development'
+              ? 'https://www.secretsibyl.com/'
+              : 'https://www.test.secretsibyl.com'
+          }`}
+          className="underline sm:text-sm text-xs"
+        >
+          {process.env.NODE_ENV === 'development'
+            ? 'Switch to mainnet'
+            : 'Switch to testnet'}
+        </a>
       </div>
-    </motion.header>
+    </>
   );
 }
