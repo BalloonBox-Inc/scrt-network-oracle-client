78e78d7f96a6dc82bf6fabdf4cf4e4b07f0bee7a
mmortelliti <matteo@balloonbox.io>
2022-05-17 16:11:31 -0400

    updated context to use reducer hook

--- src/components/TweetBtn/index.tsx
+++ src/components/TweetBtn/index.tsx
@@ -6,13 +6,18 @@ const TweetBtn = ({ message }: ITweetBtn) => {
   return (
     <div className="z-20 flex flex-col justify-center items-center mt-20">
       Share it on Twitter
-      <div className="bg-white flex justify-center rounded-full py-3 px-2.5 mt-4 cursor-pointer hover:bg-gray-300">
+      <div className="bg-white flex justify-center rounded-full py-3 px-2.5 mt-4 mb-4 cursor-pointer hover:bg-gray-300">
         <a
           href={`https://twitter.com/intent/tweet?text=${message}`}
           target="_blank"
           rel="noreferrer"
         >
-          <img src="../images/twitter-icon.svg" alt="twitter-icon" />
+          <img
+            width={'20px'}
+            height={'20px'}
+            src="../images/twitter-icon.svg"
+            alt="twitter-icon"
+          />
         </a>
       </div>
     </div>
